b7d95721bcfebf3fff0b6ac755813d1040776615
WONILLISM <wonillism@gmail.com>
2020-03-24 17:33:58 +0900

    Set movie title, rank, over view

--- ImitateNetflix/WONILLISM/src/javascripts/index.js
+++ ImitateNetflix/WONILLISM/src/javascripts/index.js
@@ -1,5 +1,22 @@
 import {APP_KEY} from "../key/appKey.js";
-//const APP_KEY = "82d29c7e28e4f090ab1cc7e20da2d4a2";
+const getAPI = Movies =>{
+    fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${APP_KEY}`,{
+            method: "GET",
+            headers:{
+
+            }
+        }
+    )
+    .then(response=>{
+        //console.log(response);
+        return response.json();
+    })
+    .then(json=>{
+        //console.log(json);
+        return json;
+    })
+}
 const getPopularMovie = popularMovie =>{
     fetch(
         `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APP_KEY}`,
@@ -16,18 +33,27 @@ const getPopularMovie = popularMovie =>{
     })
     .then(function(json){
         //console.log(json);
-        const popularMovieImg = document.querySelector('.popular_move_img');
         const popularMovies = json.results;
-        console.log(popularMovies);
+
+        const popularMovieImg = document.querySelector('.popular_movie_img');
         let r = Math.floor(Math.random()*popularMovies.length);
         // console.log(r);
         popularMovieImg.src += popularMovies[r].backdrop_path;
+
+        const popularMovieTitle = document.querySelector('.popular_movie_title h1');
+        popularMovieTitle.innerHTML = popularMovies[r].title;
+        const popularMovieRank = document.querySelector('.popular_movie_rank h2');
+        popularMovieRank.innerHTML ='Rank: '+ r;
+        const popularMovieOverview = document.querySelector('.popular_movie_overview');
+        popularMovieOverview.innerHTML = popularMovies[r].overview;
+
     })
     .catch(function(e){
         console.log(e);
     })
 }
 function init(){
+    //getMovies();
     getPopularMovie();
 }
 init();
