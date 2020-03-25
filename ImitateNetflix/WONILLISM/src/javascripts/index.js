36160175be63cd0f06240e0e49ddde7fd9324aa4
WONILLISM <wonillism@gmail.com>
2020-03-25 18:46:38 +0900

    call api test

--- ImitateNetflix/WONILLISM/src/javascripts/index.js
+++ ImitateNetflix/WONILLISM/src/javascripts/index.js
@@ -1,12 +1,9 @@
 import {APP_KEY} from "../key/appKey.js";
-const getAPI = Movies =>{
-    fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${APP_KEY}`,{
-            method: "GET",
-            headers:{
+const popularUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${APP_KEY}`;
 
-            }
-        }
+const getPopularMovies = popularMovies =>{
+    fetch(
+        popularUrl,{method: "GET", headers:{}}
     )
     .then(response=>{
         //console.log(response);
@@ -14,46 +11,24 @@ const getAPI = Movies =>{
     })
     .then(json=>{
         //console.log(json);
-        return json;
-    })
-}
-const getPopularMovie = popularMovie =>{
-    fetch(
-        `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APP_KEY}`,
-        {
-            method: "GET",
-            headers:{
-                
-            }
-        }
-    )
-    .then(function(response){
-        //console.log(response);
-        return response.json();
-    })
-    .then(function(json){
-        //console.log(json);
-        const popularMovies = json.results;
-
+        const arrMovies = json.results;
         const popularMovieImg = document.querySelector('.popular_movie_img');
-        let r = Math.floor(Math.random()*popularMovies.length);
-        // console.log(r);
-        popularMovieImg.src += popularMovies[r].backdrop_path;
-
         const popularMovieTitle = document.querySelector('.popular_movie_title h1');
-        popularMovieTitle.innerHTML = popularMovies[r].title;
         const popularMovieRank = document.querySelector('.popular_movie_rank h2');
-        popularMovieRank.innerHTML ='Rank: '+ r;
         const popularMovieOverview = document.querySelector('.popular_movie_overview');
-        popularMovieOverview.innerHTML = popularMovies[r].overview;
+        let r = Math.floor(Math.random()*arrMovies.length);
+        // console.log(r);
 
+        /* get random main page info */
+        const mainPageInfo = arrMovies[r];
+        popularMovieImg.src += mainPageInfo.backdrop_path;     
+        popularMovieTitle.innerHTML = mainPageInfo.title;
+        popularMovieRank.innerHTML ='Rank: '+ r;
+        popularMovieOverview.innerHTML = mainPageInfo.overview; 
     })
     .catch(function(e){
         console.log(e);
     })
 }
-function init(){
-    //getMovies();
-    getPopularMovie();
-}
-init();
+
+getPopularMovies();
